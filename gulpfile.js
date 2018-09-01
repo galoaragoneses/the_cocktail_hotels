b9778a5e5fb4bb03ecd3c23d070c9d2212a8bf5e
galoaragoneses <galo.aragoneses@gmail.com>
2018-09-01 09:15:46 +0200

    Gulp Sass Fix Generated File

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,10 @@
 var gulp = require('gulp');
-var sass = require('gulp-sass-bulk-import');
+var sass = require('gulp-sass');
 
 gulp.task('sass', function(){
   return gulp.src('./assets/css/src/application.scss')
     .pipe(sass())
-    .pipe(gulp.dest('./assets/css'))
+    .pipe(gulp.dest('./assets/css/'))
 });
 
 gulp.task('watch', function(){
