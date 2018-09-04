8e54c42e5e635bf65d74dabb1a7f6da61196fbb3
galoaragoneses <galo.aragoneses@gmail.com>
2018-09-04 23:01:12 +0200

    Navigation Hash Control

--- assets/js/application.js
+++ assets/js/application.js
@@ -3,5 +3,6 @@ $(document).ready(function () {
   //Load Scripts
   $.getScript('/assets/js/src/blocks/nav.js'); 
   $.getScript('/assets/js/src/blocks/tab1.js'); 
+  $.getScript('/assets/js/src/blocks/tab2.js'); 
 
 });
---
+++ assets/js/src/blocks/tab2.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+
+  var $block = $("#tab2Container");
+  if (!$block.length) return false;
+
+});
