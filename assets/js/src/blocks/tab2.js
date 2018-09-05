5d9a5d252c35b3aa2860a09631b12648d084ab33
galoaragoneses <galo.aragoneses@gmail.com>
2018-09-05 22:48:03 +0200

    Tab1 Tab2 Desing Basics

--- assets/js/src/blocks/tab2.js
+++ assets/js/src/blocks/tab2.js
@@ -2,5 +2,40 @@ $(document).ready(function () {
 
   var $block = $("#tab2Container");
   if (!$block.length) return false;
+  
+  $block.find(".optional-services-header").click(serviceHeaderClickEventFn);
+  
+  $block.find(".more-information-link").click(moreInformationLinkClickEventFn);
+  
+  function serviceHeaderClickEventFn() {    
+    var $elem = $(this),
+      isOpened = $elem.hasClass("opened"),
+      $servicesContainer = $elem.closest(".optional-services").find(".optional-service");
+    
+    if(isOpened) {
+      $elem.removeClass("opened");
+      $servicesContainer.fadeOut();
+    } else {
+      $elem.addClass("opened");
+      $servicesContainer.fadeIn();
+    }
+    
+  }
+  
+  function moreInformationLinkClickEventFn() {
+    
+    var $elem = $(this),
+      isOpened = $elem.hasClass("opened"),
+      $infoContainer = $elem.closest(".optional-service").find(".more-information-container");
+    
+    if(isOpened) {
+      $elem.removeClass("opened");
+      $infoContainer.fadeOut();
+    } else {
+      $elem.addClass("opened");
+      $infoContainer.fadeIn();
+    }
+    
+  }
 
 });
